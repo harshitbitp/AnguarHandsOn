37adf41acc3ead4a3a9ae5eb33c790ebdcb2a2de
Harshit Anand <53734636+harshitbitp@users.noreply.github.com>
2021-06-02 17:22:36 +0530

    Update app.component.spec.ts

--- Day2 and day3 and day4 combined/angular-learning/src/app/app.component.spec.ts
+++ Day2 and day3 and day4 combined/angular-learning/src/app/app.component.spec.ts
@@ -20,27 +20,5 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  /*it(`should have as title 'angular-learning'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-learning');
-  });
-*/
-// Karma Testing to validate html value displayed and value assigned in app.component.ts
- /*it(`should have same favorite movie value`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    const result = compiled.querySelector('h1').textContent;
-    console.log(result)
-    const app = fixture.componentInstance;
-    expect("Favorite Movie: " + app.favoriteMovie).toEqual(result);
-  });
-*/
-  /*it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angular-learning app is running!');
-  });*/
+ 
 });
